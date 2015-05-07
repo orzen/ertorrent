84f8ffa6da5e0e9afc1dbcfd2134586f56b54ae7
Mattias Hansson <hansson.mattias@gmail.com>
2015-05-07 21:31:05 +0200

    Prototype with magnet URI

---
+++ magnet_uri.erl
@@ -0,0 +1,10 @@
+-module(magnet_uri).
+-include("magnet_uri.hrl").
+
+-export([decode/1]).
+
+decode(Uri) ->
+    decode(Uri, #magnet_uri{}).
+
+decode(<<"magnet:?xt=urn:btih:">>) ->
+    decode(Tail, )
