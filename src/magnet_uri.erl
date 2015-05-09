b45507dc61b52a73e249ce986500a83569c3f246
Olof Johansson <olof@ethup.se>
2015-05-09 15:48:37 +0200

    Comment out unfinished functions in magnet_uri
    (Causes build errors.)

--- src/magnet_uri.erl
+++ src/magnet_uri.erl
@@ -1,10 +1,10 @@
 -module(magnet_uri).
 -include("magnet_uri.hrl").
 
--export([decode/1]).
+%-export([decode/1]).
 
-decode(Uri) ->
-    decode(Uri, #magnet_uri{}).
+%decode(Uri) ->
+%    decode(Uri, #magnet_uri{}).
 
-decode(<<"magnet:?xt=urn:btih:">>) ->
-    decode(Tail, )
+%decode(<<"magnet:?xt=urn:btih:">>) ->
+%    decode(Tail, )
