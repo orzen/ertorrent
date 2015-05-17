c9310f30e1e05950453865b73b5a0967377082ef
Mattias Hansson <hansson.mattias@gmail.com>
2015-05-17 11:14:27 +0200

    Working example of handling UDP connect request
    and response

--- src/udp_connection.erl
+++ src/udp_connection.erl
@@ -3,12 +3,12 @@
 -export([new/3]).
 
 new(Hash, Tracker_dns, Port) ->
-    %{ok, Ip_address} = inet:getaddr(Tracker_dns, inet),
-    {ok, Socket} = gen_udp:open(33000),
+    {ok, Socket} = gen_udp:open(33000, [{active, false}]),
     {_, _, Transaction_id} = random:seed(erlang:now()),
     {ok, Request} = udp_messages:new_connect_request(16#41727101980, 0, Transaction_id),
     ok = gen_udp:send(Socket, Tracker_dns, Port, Request),
-    Response = gen_udp:recv(Socket, 128, 5000),
+    %How is Response kept as binary instead of converted to int list?
+    {ok, {_, _, Response}} = gen_udp:recv(Socket, 128, 5000),
+    Response2 = udp_messages:parse_connect_response(iolist_to_binary(Response)),
     gen_udp:close(Socket),
-    Response.
-
+    {transaction_id, Transaction_id, resp, Response2}.
--- src/udp_messages.erl
+++ src/udp_messages.erl
@@ -11,15 +11,15 @@
 new_connect_request(Connection_id,
                     Action,
                     Transaction_id) ->
-    Request = <<Connection_id:64/big-signed-integer,
-                Action:32/big-signed-integer,
-                Transaction_id:32/big-signed-integer>>,
+    Request = <<Connection_id:8/big-signed-integer-unit:8,
+                Action:4/big-signed-integer-unit:8,
+                Transaction_id:4/big-signed-integer-unit:8>>,
     {ok, Request}.
 
 parse_connect_response(Response) ->
-    <<Action:32,
-      Transaction_id:32,
-      Connection_id:64>> = Response,
+    <<Action:4/big-signed-integer-unit:8,
+      Transaction_id:4/big-signed-integer-unit:8,
+      Connection_id:8/big-signed-integer-unit:8>> = Response,
     {ok, Action, Transaction_id, Connection_id}.
 
 new_announce_request(Connection_id,
@@ -35,51 +35,51 @@ new_announce_request(Connection_id,
                      Key,
                      Num_want,
                      Port) ->
-    Request = <<Connection_id:64,
-                Action:32,
-                Transaction_id:32,
-                Info_hash:160,
-                Peer_id:160,
-                Downloaded:64,
-                Left:64,
-                Uploaded:64,
-                Event:32,
-                Ip_address:32,
-                Key:32,
-                Num_want:32,
-                Port:16>>,
+    Request = <<Connection_id:8/big-signed-integer-unit:8,
+                Action:4/big-signed-integer-unit:8,
+                Transaction_id:4/big-signed-integer-unit:8,
+                Info_hash:20/big-signed-integer-unit:8,
+                Peer_id:20/big-signed-integer-unit:8,
+                Downloaded:8/big-signed-integer-unit:8,
+                Left:8/big-signed-integer-unit:8,
+                Uploaded:8/big-signed-integer-unit:8,
+                Event:4/big-signed-integer-unit:8,
+                Ip_address:4/big-signed-integer-unit:8,
+                Key:4/big-signed-integer-unit:8,
+                Num_want:4/big-signed-integer-unit:8,
+                Port:2/big-signed-integer-unit:8>>,
     {ok, Request}.
 
 parse_announce_response(Response) ->
-    <<Action:32,
-      Transaction_id:32,
-      Interval:32,
-      Leechers:32,
-      Seeders:32,
-      Ip_address:32,
-      Port:16>> = Response,
+    <<Action:4/big-signed-integer-unit:8,
+      Transaction_id:4/big-signed-integer-unit:8,
+      Interval:4/big-signed-integer-unit:8,
+      Leechers:4/big-signed-integer-unit:8,
+      Seeders:4/big-signed-integer-unit:8,
+      Ip_address:4/big-signed-integer-unit:8,
+      Port:2/big-signed-integer-unit:8>> = Response,
     {ok, Action, Transaction_id, Interval, Leechers, Seeders, Ip_address, Port}.
 
 new_scrape_request(Connection_id,
                    Action,
                    Transaction_id,
                    Info_hash) ->
-    Request = <<Connection_id:64,
-                Action:32,
-                Transaction_id:32,
-                Info_hash:160>>,
+    Request = <<Connection_id:8/big-signed-integer-unit:8,
+                Action:4/big-signed-integer-unit:8,
+                Transaction_id:4/big-signed-integer-unit:8,
+                Info_hash:20/big-signed-integer-unit:8>>,
     {ok, Request}.
 
 parse_scrape_response(Response) ->
-    <<Action:32,
-      Transaction_id:32,
-      Seeders:32,
-      Completed:32,
-      Leechers:32>> = Response,
+    <<Action:4/big-signed-integer-unit:8,
+      Transaction_id:4/big-signed-integer-unit:8,
+      Seeders:4/big-signed-integer-unit:8,
+      Completed:4/big-signed-integer-unit:8,
+      Leechers:4/big-signed-integer-unit:8>> = Response,
     {ok, Action, Transaction_id, Seeders, Completed, Leechers}.
 
 parse_error_response(Response) ->
-    <<Action:32,
-      Transaction_id:32,
-      Message>> = Response,
+    <<Action:4/big-signed-integer-unit:8,
+      Transaction_id:4/big-signed-integer-unit:8,
+      Message/big-signed-integer>> = Response,
     {ok, Action, Transaction_id, Message}.
