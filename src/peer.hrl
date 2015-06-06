31fe0fb0827b533474e78861867ee7e91ef9ead8
Mattias Hansson <hansson.mattias@gmail.com>
2015-06-06 02:03:59 +0200

    Added record for peer data

---
+++ src/peer.hrl
@@ -0,0 +1,14 @@
+%% File: peer.hrl
+%%
+%%-----------------------------
+%% Data Type: peer
+%% where:
+%%     peer_id:
+%%     ip:
+%%     port:
+%%-----------------------------
+
+-record(peer,
+        {id,
+         ip,
+         port}).
