452a206c90956e8bc9136979120af330edae5c09
Mattias Hansson <hansson.mattias@gmail.com>
2016-02-13 15:38:16 +0100

    Specified the base of the ID values

--- src/peer_protocol_message_id.hrl
+++ src/peer_protocol_message_id.hrl
@@ -1,12 +1,12 @@
 %% File: peer_protocol_message_id.hrl
 % Message IDs
--define(CHOKE, 0).
--define(UNCHOKE, 1).
--define(INTERESTED, 2).
--define(NOT_INTERESTED, 3).
--define(HAVE, 4).
--define(BITFIELD, 5).
--define(REQUEST, 6).
--define(PIECE, 7).
--define(CANCEL, 8).
--define(PORT, 9).
+-define(CHOKE, 0:8).
+-define(UNCHOKE, 1:8).
+-define(INTERESTED, 2:8).
+-define(NOT_INTERESTED, 3:8).
+-define(HAVE, 4:8).
+-define(BITFIELD, 5:8).
+-define(REQUEST, 6:8).
+-define(PIECE, 7:8).
+-define(CANCEL, 8:8).
+-define(PORT, 9:8).
