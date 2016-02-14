35cb13e7da06afed9a4eedc7c178d95d55f0e4ee
Mattias Hansson <hansson.mattias@gmail.com>
2016-02-14 17:27:14 +0100

    Replaced parse_file with utility functions
    The utility functions will parse values upon request instead of making
    assumptions about which values the metainfo may contain.

--- src/metainfo.erl
+++ src/metainfo.erl
@@ -1,52 +1,32 @@
 -module(metainfo).
--include("metainfo.hrl").
 
--export([parse_file/1, parse_magnet/1]).
+-export([read_file/1,
+         get_value/2,
+         get_info_value/2,
+         parse_magnet/1]).
 
-parse_file(Filename) ->
+-import(lists, [map/2]).
+
+read_file(Filename) ->
     {ok, Data} = file:read_file(Filename),
     {ok, {dict, Total}} = bencode:decode(Data),
-    {ok, parse_file(Total, #metainfo{})}.
+    {ok, Total}.
 
-parse_file([], Record) ->
-    Record;
-parse_file([{<<"announce">>, Value}|Tail], Record) ->
-    New_record = Record#metainfo{announce=Value},
-    parse_file(Tail, New_record);
-parse_file([{<<"announce-list">>, Value}|Tail], Record) ->
-    New_record = Record#metainfo{announce_list=Value},
-    parse_file(Tail, New_record);
-parse_file([{<<"comment">>, Value}|Tail], Record) ->
-    New_record = Record#metainfo{comment=Value},
-    parse_file(Tail, New_record);
-parse_file([{<<"creation date">>, Value}|Tail], Record) ->
-    New_record = Record#metainfo{creation_date=Value},
-    parse_file(Tail, New_record);
-parse_file([{<<"httpseeds">>, Value}|Tail], Record) ->
-    New_record = Record#metainfo{httpseeds=Value},
-    parse_file(Tail, New_record);
-parse_file([{<<"info">>, Value}|Tail], Record) ->
-    {dict, Info_data} = Value,
-    {ok, Info_bencoded} = bencode:encode(Value),
-    Info_record = parse_info(Info_data, #info{}),
-    {ok, Info_encoded} = utils:encode_hash(Info_bencoded),
-    New_record = Record#metainfo{info_hash=Info_encoded, info=Info_record},
-    parse_file(Tail, New_record).
+get_value(Key, Metainfo) ->
+    case lists:keyfind(Key, 1, Metainfo) of
+        {_, Value} ->
+            {ok, Value};
+        false ->
+            {error, no_match}
+    end.
 
-parse_info([], Record) ->
-    Record;
-parse_info([{<<"length">>, Value}|Tail], Record) ->
-    New_record = Record#info{length=Value},
-    parse_info(Tail, New_record);
-parse_info([{<<"name">>, Value}|Tail], Record) ->
-    New_record = Record#info{name=Value},
-    parse_info(Tail, New_record);
-parse_info([{<<"piece length">>, Value}|Tail], Record) ->
-    New_record = Record#info{piece_length=Value},
-    parse_info(Tail, New_record);
-parse_info([{<<"pieces">>, Value}|Tail], Record) ->
-    New_record = Record#info{pieces=Value},
-    parse_info(Tail, New_record).
+get_info_value(Key, Metainfo) ->
+    case get_value(<<"info">>, Metainfo) of
+        {_, {dict, Info_entry}} ->
+            get_value(Key, Info_entry);
+        {error, no_match} ->
+            {error, no_info_entry}
+    end.
 
 parse_magnet(Uri) ->
     Urn = string:substr(Uri, 9),
