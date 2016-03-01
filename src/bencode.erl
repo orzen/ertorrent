8b22c3c3f67543470b0a5b9527b797b6b4771c20
Mattias Hansson <hansson.mattias@gmail.com>
2016-03-01 21:53:46 +0100

    Rewrite bencode module
    Now support more generic datatypes earlier '{dict, [{Key, Value}]}'
    defined a dict. now a dict is defined as '[{Key, Value}]'. The rewritten
    module also contain a function to decode from file.

--- src/bencode.erl
+++ src/bencode.erl
@@ -1,158 +1,194 @@
--module(bencode).
+-module(bencode_alt).
 
--export([decode/1, encode/1]).
+-export([decode_file/1,
+         decode/1,
+         encode/1]).
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
 -endif.
 
+decode_file(Filename) ->
+    {ok, Data} = file:read_file(Filename),
+    decode(Data).
+
 decode(Data) ->
-    Binary_data = iolist_to_binary(Data),
-    case dec(Binary_data) of
-        {{dict, Value}, _} -> Return = {dict, Value};
-        {{list, Value}, _} -> Return = {list, Value};
-        {Value, _} -> Return = Value
-    end,
-    {ok, Return}.
+    [Decoded] = decode(iolist_to_binary(Data), []),
+    {ok, Decoded}.
 
-encode(Struct) ->
-    {ok, iolist_to_binary(enc(Struct))}.
+%% Done parsing
+decode(<<"">>, Acc) ->
+    lists:reverse(Acc);
 
-dec(<<$d, Tail/binary>>) ->
-    dec_dict(Tail, []);
-dec(<<$l, Tail/binary>>) ->
-    dec_list(Tail, []);
-dec(<<$i, Tail/binary>>) ->
-    dec_int(Tail, []);
-dec(Data) ->
-    dec_string(Data, []).
+%% Reached the end of a container type
+decode(<<$e, Tail/binary>>, Acc) ->
+    {Tail, lists:reverse(Acc)};
 
-dec_int(<<$e, Tail/binary>>, Acc) ->
-    {list_to_integer(lists:reverse(Acc)), Tail};
-dec_int(<<X, Tail/binary>>, Acc) ->
-    dec_int(Tail, [X|Acc]).
+%% Parsing integer
+decode(<<$i, Tail/binary>>, Acc) ->
+    {Int, New_tail} = parse_int(Tail, 0),
+    decode(New_tail, [Int|Acc]);
 
-dec_string(<<$:, Tail/binary>>, Acc)->
-    Int = list_to_integer(lists:reverse(Acc)),
-    <<Str:Int/binary, Rest/binary>> = Tail,
-    {Str, Rest};
-dec_string(<<X, Tail/binary>>, Acc) ->
-    dec_string(Tail, [X|Acc]).
+%% Parsing list
+decode(<<$l, Tail/binary>>, Acc) ->
+    {New_tail, List} = decode(Tail, []),
+    decode(New_tail, [List|Acc]);
 
-dec_list(<<$e, Tail/binary>>, Acc) ->
-    {{list, lists:reverse(Acc)}, Tail};
-dec_list(Data, Acc) ->
-    {Res, Tail} = dec(Data),
-    dec_list(Tail, [Res|Acc]).
+%% Parsing dict
+decode(<<$d, Tail/binary>>, Acc) ->
+    % Treat the dict as a list and then convert it properly
+    {New_tail, List} = decode(Tail, []),
+    {ok, Dict} = list_to_dict(List, []),
+    decode(New_tail, [Dict|Acc]);
 
-dec_dict(<<$e, Tail/binary>>, Acc) ->
-    {{dict, lists:reverse(Acc)}, Tail};
-dec_dict(Data, Acc) ->
-    {Key, Tail1} = dec(Data),
-    {Value, Tail2} = dec(Tail1),
-    H = {Key, Value},
-    dec_dict(Tail2, [H|Acc]).
+%% Parsing string
+decode(<<Any/binary>>, Acc)  ->
+    {Length, Int_tail} = parse_int(Any, 0),
+    <<String:Length/binary, String_tail/binary>> = Int_tail,
+    decode(String_tail, [String|Acc]).
 
+encode(Term) ->
+    {ok, encode_term(Term)}.
 
-enc(Int) when is_integer(Int) ->
-    IntBin = list_to_binary(integer_to_list(Int)),
-    [$i, IntBin, $e];
-enc(Str) when is_list(Str) ->
-    enc(list_to_binary(Str));
-enc(Str) when is_binary(Str) ->
-    IntBin = list_to_binary(integer_to_list(size(Str))),
-    [IntBin, $:, Str];
-enc(Tuple) when is_tuple(Tuple), (element(1, Tuple) /= dict), (element(1, Tuple) /= list) ->
-    {Key, Value} = Tuple,
-    [enc(Key), enc(Value)];
-enc({list, List}) when is_list(List) ->
-    [$l, [enc(Elem) || Elem <- List], $e];
-enc({dict, Dict}) when is_list(Dict)->
-    [$d, [enc(Elem) || Elem <- Dict], $e].
+%% Encode integer
+encode_term(H) when is_integer(H) ->
+    Binary_integer = integer_to_binary(H),
+    list_to_binary([$i, Binary_integer, $e]);
 
-% Tests
+%% Encode string
+encode_term(H) when is_bitstring(H) ->
+    Length = byte_size(H),
+    Binary_length = integer_to_binary(Length),
+    list_to_binary([Binary_length, $:, H]);
+
+%% Determine the container type
+encode_term(H) when is_list(H) ->
+    case is_dict(H) of
+        true ->
+            encode_dict(H, []);
+        false ->
+            encode_list(H, [])
+    end.
+
+%% Encode dict
+encode_dict([], Acc) ->
+    Reverse_dict = lists:reverse([<<$e>>|Acc]),
+    list_to_binary([<<$d>>|Reverse_dict]);
+encode_dict([{Key, Value}|Tail], Acc) ->
+    Encoded_key = encode_term(Key),
+    Encoded_value = encode_term(Value),
+    encode_dict(Tail, [Encoded_value, Encoded_key|Acc]).
+
+%% Encode list
+encode_list([], Acc) ->
+    Reverse_list = lists:reverse([<<$e>>|Acc]),
+    list_to_binary([<<$l>>|Reverse_list]);
+encode_list([H|Tail], Acc) ->
+    Encoded_value = encode_term(H),
+    encode_list(Tail, [Encoded_value|Acc]).
+
+%%% UTILITY FUNCTIONS
+
+%% Validate if term is dict()
+%% dict() -> [{key1, value1}, {key2, value2}}]
+is_dict([]) ->
+    true;
+is_dict([H|Tail]) ->
+    case is_tuple(H)
+         andalso tuple_size(H) =:= 2
+         andalso is_bitstring(element(1, H)) of
+        true -> is_dict(Tail);
+        false -> false
+    end.
+
+%% Parsing both integer value and string prefix
+parse_int(<<$e, Tail/binary>>, Acc) ->
+    {Acc, Tail};
+parse_int(<<$:, Tail/binary>>, Acc) ->
+    {Acc, Tail};
+parse_int(<<Ascii_nbr/integer, Tail/binary>>, Acc) ->
+    Length_part = Ascii_nbr-$0,
+    case Length_part >= 0 andalso Length_part =< 9 of
+        true ->
+            parse_int(Tail, Acc * 10  + Length_part);
+        false ->
+            {error, "failed to parse"}
+    end.
+
+%% Convert list to dict()
+%% dict() -> [{key1, value1}, {key2, value2}}]
+list_to_dict([], Acc) ->
+    {ok, lists:reverse(Acc)};
+list_to_dict([Key, Value|Tail], Acc) ->
+    list_to_dict(Tail, [{Key, Value}|Acc]).
+
+%%% Tests
 -ifdef(TEST).
 
-%Test decoding list nested inside a dict
+% Test decoding list nested inside a dict
 decode_nested_1_test() ->
-    erlang:display("Test decode nested 1"),
     {ok, Dict} = decode(<<"d3:key5:value3:fool3:bar3:bazee">>),
-    ?assert(Dict =:= {dict,
-                      [{<<"key">>, <<"value">>},
-                       {<<"foo">>, {list, [<<"bar">>, <<"baz">>]}}]}).
+    ?assert(Dict =:= [{<<"key">>, <<"value">>}, {<<"foo">>, [<<"bar">>,
+                                                             <<"baz">>]}]).
 
-%Test decoding dict nested inside a list
+% Test decoding dict nested inside a list
 decode_nested_2_test() ->
-    erlang:display("Test decode nested 2"),
     {ok, List} = decode(<<"ld3:foo3:baree">>),
-    ?assert(List =:= {list, [{dict, [{<<"foo">>, <<"bar">>}]}]}).
+    ?assert(List =:= [[{<<"foo">>, <<"bar">>}]]).
 
-%Test decoding plain dict
+% Test decoding plain dict
 decode_dict_test() ->
-    erlang:display("Test decode dict"),
     {ok, Dict} = decode(<<"d3:key5:value3:foo3:bare">>),
-    ?assert(Dict =:= {dict, [{<<"key">>, <<"value">>}, {<<"foo">>, <<"bar">>}]}).
+    ?assert(Dict =:= [{<<"key">>, <<"value">>}, {<<"foo">>, <<"bar">>}]).
 
-%Test decoding plain list
+% Test decoding plain list
 decode_list_test() ->
-    erlang:display("Test decode list"),
     {ok, List} = decode(<<"l3:foo3:bare">>),
-    ?assert(List =:= {list, [<<"foo">>, <<"bar">>]}).
+    ?assert(List =:= [<<"foo">>, <<"bar">>]).
 
-%Test decoding plain integer
+% Test decoding plain integer
 decode_int_test() ->
-    erlang:display("Test decode integer"),
     {ok, Integer} = decode(<<"i42e">>),
     ?assert(Integer =:= 42).
 
-%Test decoding plain string
+% Test decoding plain string
 decode_string_test() ->
-    erlang:display("Test decode string"),
     {ok, String} = decode(<<"3:foo">>),
     ?assert(String =:= <<"foo">>).
 
-%Test encoding a list nested in a dict
+% Test encoding a list nested in a dict
 encode_nested_1_test() ->
-    erlang:display("Test encode nested 1"),
-    Dict = {dict,
-            [{<<"key">>, <<"value">>},
-             {<<"foo">>, {list, [<<"bar">>, <<"baz">>]}}]},
+    Dict = [{<<"key">>, <<"value">>}, {<<"foo">>, [<<"bar">>, <<"baz">>]}],
     {ok, String} = encode(Dict),
     ?assert(String =:= <<"d3:key5:value3:fool3:bar3:bazee">>).
 
-%Test encoding a dict nested in a list
+% Test encoding a dict nested in a list
 encode_nested_2_test() ->
-    erlang:display("Test encode nested 2"),
-    List = {list, [{dict, [{<<"foo">>, <<"bar">>}]}]},
+    List = [[{<<"foo">>, <<"bar">>}]],
     {ok, String} = encode(List),
     ?assert(String =:= <<"ld3:foo3:baree">>).
 
-%Test encoding plain dict
+% Test encoding plain dict
 encode_dict_test() ->
-    erlang:display("Test encode dict"),
-    Dict = {dict, [{<<"key">>, <<"value">>}, {<<"foo">>, <<"bar">>}]},
+    Dict = [{<<"key">>, <<"value">>}, {<<"foo">>, <<"bar">>}],
     {ok, String} = encode(Dict),
     ?assert(String =:= <<"d3:key5:value3:foo3:bare">>).
 
-%Test encoding plain list
+% Test encoding plain list
 encode_list_test() ->
-    erlang:display("Test encode list"),
-    List = {list, [<<"foo">>, <<"bar">>]},
+    List = [<<"foo">>, <<"bar">>],
     {ok, String} = encode(List),
     ?assert(String =:= <<"l3:foo3:bare">>).
 
-%Test encoding plain integer
+% Test encoding plain integer
 encode_int_test() ->
-    erlang:display("Test encode integer"),
     Integer = 42,
     {ok, String} = encode(Integer),
     ?assert(String =:= <<"i42e">>).
 
-%Test encoding plain string
+% Test encoding plain string
 encode_string_test() ->
-    erlang:display("Test encode string"),
-    Str = "foobar",
-    {ok, String} = encode(Str),
+    {ok, String} = encode(<<"foobar">>),
     ?assert(String =:= <<"6:foobar">>).
 
 -endif.
