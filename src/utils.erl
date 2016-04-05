b4f8ec3e1f9ed01786f6b121709f5409639cb41e
Mattias Hansson <hansson.mattias@gmail.com>
2016-04-05 07:37:29 +0200

    utils: Fix coding style

--- src/utils.erl
+++ src/utils.erl
@@ -16,11 +16,12 @@ format_hash([Msn, Lsn|Tail], Acc) ->
     format_hash(Tail, New_acc).
 
 encode_hash(Info_bencoded) ->
-    %160bits/8=20 byte SHA1 as integerlist
+    % 160bits/8=20 byte SHA1 as integerlist
     <<Hash:160/integer>> = crypto:hash(sha, Info_bencoded),
-    %%Convert the integerlist to a string with len:40, base:16, type:binary
+    % Convert the integerlist to a string with len:40, base:16, type:binary
     Info_hash = lists:flatten(io_lib:format("~40.16.0b", [Hash])),
-    {ok, format_hash(Info_hash, [])}.
+    % Percent-formatting
+    Percent_format = format_hash(Info_hash, []),
 
 is_magnet(Str) ->
     case string:str(Str, "magnet:?") == 1 of
