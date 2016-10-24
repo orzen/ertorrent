eeaa78a1b15b10b3c96c4909f630ad0959ade1ed
Mattias Hansson <hansson.mattias@gmail.com>
2016-10-24 21:55:43 +0200

    fixup! ertorrent_tracker_request: Renamed ertorrent_tracker_protocol REDO AS

--- src/ertorrent_tracker_request.erl
+++ src/ertorrent_tracker_request.erl
@@ -1,23 +1,6 @@
-%%%%%%---------------------------------------------------------------------
-%%%%%% Description module http_get_request
-%%%%%%---------------------------------------------------------------------
-%%%%%% This module contains everything necessary to perform a HTTP tracker
-%%%%%% GET request and handle its response. This module supports both basic
-%%%%%% and compact mode (explained in BEP 23).
-%%%%%%
-%%%%%% Links:
-%%%%%% http://bittorrent.org/beps/bep_0003.html
-%%%%%% http://www.bittorrent.org/beps/bep_0023.html
-%%%%%%---------------------------------------------------------------------
-%%%%%% Exports
-%%%%%%---------------------------------------------------------------------
-%%%%%% send(Metainfo, Port, Uploaded, Downloaded, Event)
-%%%%%%   Returns the response of a performed GET request with the given
-%%%%%%   metainfo.
-%%%%%%---------------------------------------------------------------------
--module(ertorrent_tracker_protocol).
+-module(ertorrent_tracker_request).
 
--export([send/1, new_request/9]).
+-export([new_request/9]).
 
 new_request(Announce_address, Info_hash, Peer_id, Port, Uploaded, Downloaded,
             Left, Event, Compact) ->
@@ -25,20 +8,3 @@ new_request(Announce_address, Info_hash, Peer_id, Port, Uploaded, Downloaded,
                   Info_hash, "&peer_id=", Peer_id, "&port=", Port,
                   "&uploaded=", Uploaded, "&downloaded=", Downloaded, "&left=",
                   Left, "&event=", Event, "&compact=", Compact]).
-
-send(Request) ->
-    inets:start(),
-    try
-        {ok, {{_, Code, _}, _, Response}} = httpc:request(get,
-                                                          {Request,
-                                                           [{"Accept",
-                                                             "text/plain"}]},
-                                                          [],
-                                                          [{sync, true},
-                                                           {headers_as_is, true}]),
-        {ok, Code, Response}
-    catch
-        Exception:Reason -> {error, Exception, Reason}
-    after
-        inets:stop()
-    end.
